cb4fb1804d2db94110fb05979c5480ef91c27789
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2022-10-19 10:00:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Display the document title in the console
+Change the document title to Modifying the DOM
+Change the background color of the body to hot pink (#FF69B4). If that worked, try with a random color as an extra challenge.
+ (tip: You can use the rgb() notation and 3 random-generated numbers between 0 and 255).
+Using the children method and a for ... of loop, display every children elements of the second child 
+element of your document (display all children elements of the <body>)
+*/
+
+
+document.getElementById("content").style.display ="block";
+var title = document.getElementsByTagName("title").innerHTML = "Modifying the DOM";
+document.body.style.backgroundColor = "rgb(256, 57, 85)";
+const collection = document.getElementById("mySelect").children[1].;
+for (children.bodyChildren) {
+    console.log()
+}
