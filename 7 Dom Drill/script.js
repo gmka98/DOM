2b7500dbbc3f7ccb420a8efa3ce7ce791d8990cb
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2022-12-19 01:41:21 +0100

    Add files via upload

---
+++ 7 Dom Drill/script.js
@@ -0,0 +1,59 @@
+const UlList = document.querySelector("ul").childNodes
+
+// use textcontent to check the text inside the li console.log
+
+for(li of UlList){
+   if(li.textContent === "Fast and Furious") {
+    li.classList.add("important");
+    if(li.parentNode.firstChild != li)
+        li.parentNode.insertBefore(li,li.parentNode.firstChild)
+        }
+};
+
+const ul = document.querySelector("ul") 
+
+document.ul.addEventListener('click', e) => {
+   if(li.parentNode.firstChild === li){
+      alert(li);
+   }
+   if(li.parentNode.firstChild === li)
+    alert("The most important franchise ever, the story of DOM(inic) Toretto's family. It's not about car, it's about family.")
+}
+
+function bye (x,y){
+   var item1 = document.querySelectorAll("ul")
+   if (var check = item1[2].isEqualNode(item1[10])){
+      item1.removeChild(item1.lastElementChild);
+   }
+}
+
+document.body.addEventListener('keyup', (event) =>){
+   if( event.key === 'r'){
+     let RandomList = Math.floor(Math.random(UlList)*89956312)
+   case 0:
+      let RandomList = li[0]
+      continue;
+   }
+   if(event.key === "d"){
+      document.querySelector("li").appendChild(clone);
+
+   }
+}
+
+const dav = document.createElement("div");
+const target = document.querySelector('ul')
+target.parentNode.insertBefore(dav, target);
+const selection = document.createElement("select")
+const atte = document.createElement("option")
+atte.classList.add("important franchises")
+const ntion = document.createElement("option")
+const ntion = element.classList;
+UlList.add("normal franchise")
+console.log(atte.textContent)
+
+document.selection.addEventListener('change' e) => {
+   if(selection.firstChild === atte){
+      atte.style.visibility = "hidden"
+   }
+}
+ 
