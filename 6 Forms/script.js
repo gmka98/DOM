c98244e5f9c555e3a0ab1c06868e609ac9c02e9e
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2022-12-19 01:39:33 +0100

    Add files via upload

---
+++ 6 Forms/script.js
@@ -0,0 +1,46 @@
+
+document.addEventListener("keyup", function(event)){
+    Event.preventDefault();
+    if (Event.keycode < 18){
+        foo();
+    }
+
+}
+
+function foo(){
+    console.log("")
+}
+
+<input type="text"></input>
+onkeydown = "event.stoptPropagation()"
+onkeyup = "event.stopPropagation()"
+onkeypress = "event.stopPropagation()">
+
+const first = document.querySelector('#display-firstname')
+const input = document.querySelector('input')
+first.append(first)
+
+document.addEventListener("keyup", function(event)){
+    Event.preventDefault();
+    if (Event.keycode > 18){
+        boo();
+    }
+
+ function boo(){
+    console.log("")
+ }
+}
+
+<input class="2" type="text"></input>
+const second = document.querySelector('#a-hard-truth')
+const input-2 = document.querySelector('input')
+second.append(input-2)
+
+document.addEventListener("keyup", function(event)){
+    if(password > 6){
+        style.color = "red" 
+    }
+
+
+change.addEventListener
+
